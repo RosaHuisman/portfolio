c8389bfafbb9deef1cb2781ca80bf83729a3e5e2
Rosa Huisman <rosa.huisman@gmail.com>
2021-09-13 14:21:08 +0200

    WIP intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -16,11 +16,16 @@ const onClickImage = () => {
   return (
         <div className="intro">
           <div className="intro__background"> 
-            <div className={ isOpen ? "intro__text__open" : "intro__text" }>
-            Ut sit amet euismod diam. Nunc est enim, imperdiet ut justo sed, fermentum lacinia urna. Phasellus tristique scelerisque dapibus. Mauris elit lectus, laoreet quis ante sed, tempor tincidunt sapien. In auctor finibus aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. 
-            </div>
-            
+
             <img src={me} className={ isOpen ? "intro__image__open" : "intro__image" } alt="Rosa Huisman" onClick={onClickImage}/>
+           
+            <p className={ isOpen ? "intro__text__open" : "intro__text" }>
+            Ut sit amet euismod diam. Nunc est enim, imperdiet ut justo sed, fermentum lacinia urna. Phasellus tristique scelerisque dapibus. Mauris elit lectus, laoreet quis ante sed, tempor tincidunt sapien. In auctor finibus aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per incepsdqftos himenaeos. Class aptent taciti. Class aptent taciti, Class aptent taciti , Cqdsdsclass aptentsdqf taciti,
+            </p>
+            
+          
+
+           
           </div >
         </div>
        
