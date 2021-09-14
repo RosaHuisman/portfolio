852096a7690dca6145769307043693aae5554e15
Rosa Huisman <rosa.huisman@gmail.com>
2021-09-14 10:24:30 +0200

    merge first section

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import './style.scss';
 
 import Intro from '../../containers/Intro'
+import Header from '../../components/Header'
+
 
 function App() {
 
   return (
     <div className="app">
+      <Header />
       <Intro />
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './style.scss';
+
+
+
+const Header = ({
+
+}) => {
+
+  return (
+        <div className="header">
+           
+          bonjour
+
+        </div>
+       
+  );
+}
+
+
+
+export default Header;
