d874cbc2d2cffd51523a0a853503900373c3cf8e
Rosa Huisman <rosa.huisman@gmail.com>
2021-09-16 15:30:38 +0200

    overlay image

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -34,28 +34,21 @@ const onClickImage = () => {
 
   return (
         <div className="intro">
-           
-          
 
-
-
-          <div className="intro__background"> 
+          <div className="intro__container"> 
             
             <div className={ isOpen ? "intro__imghello__open" : "intro__imghello" }>
               
               <h1 className={ isOpen ? "intro__title__open" : "intro__title" } > Hello world </h1>
               
-              <div className="container">
-
+              <div className="test"> 
                 <img src={me} className={ isOpen ? "intro__image__open" : "intro__image" } alt="Rosa Huisman" onClick={onClickImage} />   
-                <div className="overlay">
-                <div className="text">About me ...</div>
-                </div>
+                <p className="text">About me ...</p>
               </div>
 
             </div>
 
-            <p className="intro__clickonme">Click on me ... </p>
+       
 
             <p className={ isOpen ? "intro__text__open" : "intro__text" }>
             Ut sit amet euismod diam. Nunc est enim, imperdiet ut justo sed, fermentum lacinia urna. Phasellus tristique scelerisque dapibus. Mauris elit lectus, laoreet quis ante sed, tempor tincidunt sapien. In auctor finibus aliquam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per incepsdqftos himenaeos. Class aptent taciti. Class aptent taciti, Class aptent taciti , Cqdsdsclass aptentsdqf taciti,
