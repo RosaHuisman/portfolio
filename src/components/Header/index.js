48c1ec5f868d5e89e8ba1fc65b8cb0ebc9959e55
Rosa Huisman <rosa.huisman@gmail.com>
2021-09-17 16:01:58 +0200

    colors and others

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,9 @@ import TextTransition, { presets } from "react-text-transition";
 const TEXTS = [
   " Rosa Huisman",
   " développeuse Junior",
+  " Rosa Huisman",
   " développeuse FullStack",
+  " Rosa Huisman",
   " développeuse Front-End"
 ];
 
@@ -32,8 +34,8 @@ React.useEffect(() => {
       
 
           <Dropdown icon='bars' className="header__firstsection__dropdown">
-            <Dropdown.Menu className="header__dropdownmenu">
-              <Dropdown.Item text='Accueil' />
+            <Dropdown.Menu className="header__firstsection__dropdownmenu">
+              <Dropdown.Item text='Accueil' className="header__firstsection__dropdownmenu"/>
               <Dropdown.Item text='Expérience' />
               <Dropdown.Item text='Formation' />
               <Dropdown.Item text='Projets' />
@@ -42,24 +44,24 @@ React.useEffect(() => {
           </Dropdown>
 
           <div className="header__firstsection__social">
-            <a href="http://scripteden.com/download/eden-ui/" target="_blank" rel="noreferrer" className="btn-social btn-facebook"><i className="fa fa-facebook"></i></a>
               <a href="http://scripteden.com/download/eden-ui/" target="_blank" rel="noreferrer" className="btn-social btn-github"><i className="fa fa-github-alt"></i></a>
               <a href="http://scripteden.com/download/eden-ui/" target="_blank" rel="noreferrer" className="btn-social btn-email"><i className="fa fa-envelope"></i></a>
               <a href="http://scripteden.com/download/eden-ui/" target="_blank" rel="noreferrer" className="btn-social btn-linkedin"><i className="fa fa-linkedin"></i></a>
 
           </div>
-          
+
           </div>
 
           <div className="header__secondsection"> 
 
             
             <h1 className="header__secondsection__textanimation" > 
-            Je suis&nbsp; 
-            <TextTransition
-            text={ TEXTS[index % TEXTS.length] }
-            springConfig={ presets.wobbly }
-            />
+              <div className="header__secondsection__textanimation__iam"> Je suis&nbsp; </div>
+              <TextTransition
+              text={ TEXTS[index % TEXTS.length] }
+              springConfig={ presets.wobbly }
+              className="header__secondsection__textanimation__designation"
+              />
             </h1>
 
 
