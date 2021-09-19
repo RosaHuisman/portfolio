627b89d298d74b36b9ceab00618bdf06d8220737
rosa huisman <rosa.huisman@gmail.com>
2021-09-19 23:37:02 +0200

    cv

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.scss';
 
 import me from '../../images/me.jpg'
-import cv from '../../images/me.jpg'
+import cv from '../../images/cv image.png'
 
 
 const Intro = ({
@@ -58,8 +58,13 @@ const handleClickCV = () => {
 
           <div className="intro__cv">
             <button className={cvOpen ? "intro__cv__button__open" : "intro__cv__button" } onClick={handleClickCV}> Mon CV </button>
-            <img src={cv} className={cvOpen ? "intro__cv__button__cvopen" : "intro__cv__close" } /> 
+            <img src={cv} className={cvOpen ? "intro__cv__cvopen" : "intro__cv__close" } />
+            <button className={cvOpen ? "intro__cv__closebutton__open" : "intro__cv__closebutton" } onClick={handleClickCV}> <i class="close icon"></i></button> 
+            <form method="get" action={cv}>
+            <button className={cvOpen ? "intro__cv__dlbutton__open" : "intro__cv__dlbutton" } type="submit"><i class="download icon"></i></button>
+          </form>
           </div>
+          
 
         </div>
        
