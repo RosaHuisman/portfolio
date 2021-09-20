255a8c51af8277c4613d184cd747f4f919f129df
rosa huisman <rosa.huisman@gmail.com>
2021-09-20 10:21:50 +0200

    cv amelioration and css

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './style.scss';
 
 import me from '../../images/me.jpg'
-import cv from '../../images/cv image.png'
+import cv from '../../images/CV_image.jpg'
 
 
 const Intro = ({
@@ -59,9 +59,9 @@ const handleClickCV = () => {
           <div className="intro__cv">
             <button className={cvOpen ? "intro__cv__button__open" : "intro__cv__button" } onClick={handleClickCV}> Mon CV </button>
             <img src={cv} alt="CV de Rosa Huisman" className={cvOpen ? "intro__cv__cvopen" : "intro__cv__close" } />
-            <button className={cvOpen ? "intro__cv__closebutton__open" : "intro__cv__closebutton" } onClick={handleClickCV}> <i class="close icon"></i></button> 
+            <button className={cvOpen ? "intro__cv__closebutton__open" : "intro__cv__closebutton" } onClick={handleClickCV}> <i className="close icon"></i></button> 
             <form method="get" action={cv}>
-            <button className={cvOpen ? "intro__cv__dlbutton__open" : "intro__cv__dlbutton" } type="submit"><i class="download icon"></i></button>
+            <button className={cvOpen ? "intro__cv__dlbutton__open" : "intro__cv__dlbutton" } type="submit"><i className="download icon"></i></button>
           </form>
           </div>
           
